c18809f2bc7a5a176c0c2d4f677fc79c14a6c4a8
KimYeongJo <132258199+KimYeongJo@users.noreply.github.com>
2023-08-17 18:06:02 +0900

    🚧 WIP: Post models.py, serializers.py 작성

--- myapp/settings.py
+++ myapp/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     #Installed package
-    'rest-framework',
+    'rest_framework',
     #CustomApp
     'users',
 ]
