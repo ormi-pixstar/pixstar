8fda17e1e0e07712bd44265c2018f66011d4309d
LJS0000 <jisu2281l@naver.com>
2023-08-18 15:06:37 +0900

    ⚡️ IMPRV: 검색 view 고도화
    1. 기능별 함수 분리
    2. response값에 likes 개수 추가

--- posts/views.py
+++ posts/views.py
@@ -8,22 +8,31 @@ from django.db.models import Count
 
 # 포스트 조회 및 검색
 class PostList(APIView):
+    # 검색 쿼리 처리
+    def search_posts(self, search_query):
+        if search_query:
+            return Post.objects.filter(content__icontains=search_query)
+        return Post.objects.all()
+
+    # 정렬 쿼리 처리
+    def order_posts(self, posts, sort_order):
+        if sort_order == 'asc':
+            return posts.order_by('created_at')
+        elif sort_order == 'likes':
+            return posts.order_by('-likes_count')
+        return posts.order_by('-created_at')
+
     def get(self, request):
         # 쿼리 파라미터 가져오기
         search_query = request.query_params.get('search', None)
-        sort_order = request.query_params.get('sort', None)
+        sort_order = request.query_params.get('sort', 'desc')
 
-        # 검색 쿼리 처리
-        if search_query:
-            posts = Post.objects.filter(content__icontains=search_query)
-        else:
-            posts = Post.objects.all()
-
-        # 정렬 쿼리 처리
-        if sort_order == 'desc':
-            posts = posts.order_by('-created_at')
-        elif sort_order == 'likes':
-            posts = posts.annotate(likes_count=Count('like')).order_by('-likes_count')
+        # 검색어에 맞는 포스트 가져오기
+        posts = self.search_posts(search_query)
+        # likes_count를 response에 언제나 포함
+        posts = posts.annotate(likes_count=Count('like'))
+        # 조건에 맞춰서 정렬
+        posts = self.order_posts(posts, sort_order)
 
         serializer = PostSerializer(posts, many=True)
 
