d2693f727ed5e01e50f50e300bd2aa3235f94f6e
LJS0000 <jisu2281l@naver.com>
2023-08-18 16:44:13 +0900

    ✨ FEATURE: Swagger 적용

--- myapp/urls.py
+++ myapp/urls.py
@@ -16,10 +16,17 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # Swagger
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path(
+        'api/schema/docs/',
+        SpectacularSwaggerView.as_view(url_name='schema'),
+        name='swagger-ui',
+    ),
     # path('users/', include('users.urls')),
     path('api/', include('posts.urls')),
 ]
