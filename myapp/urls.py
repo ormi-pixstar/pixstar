8f6b04e7e9d5adfdabcdd66d6c5c2ad91eb27bad
LJS0000 <jisu2281l@naver.com>
2023-08-21 16:22:37 +0900

    ✏️ FIX: import 수정

--- myapp/urls.py
+++ myapp/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 urlpatterns = [
