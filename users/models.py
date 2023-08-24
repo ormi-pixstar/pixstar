3d17e5fb1d18e0950021f8e4a679fd4b0adecc26
tosecformback <sub7503@naver.com>
2023-08-24 15:01:39 +0900

    ✏️ FIX: UsersModel 수정

--- users/models.py
+++ users/models.py
@@ -6,13 +6,13 @@ from django.utils import timezone
 
 class UserManager(BaseUserManager):
 
-    def _create_user (self, nickname, email,  password, is_staff, is_superuser, **extra_fields):
+    def _create_user (self, username, email,  password, is_staff, is_superuser, **extra_fields):
         if not email:
             raise ValueError("User must have an E-mail")
         now = timezone.localtime()
         email = self.normalize_email(email)
         user = self.model(
-            nickname = nickname,
+            username = username,
             email = email,
             is_staff = is_staff,
             is_superuser = is_superuser,
@@ -26,17 +26,17 @@ class UserManager(BaseUserManager):
         user.save(using=self._db)
         return user
     
-    def create_user(self, nickname, email, password, **extra_fields):
-        return self._create_user(nickname, email, password, False, False, **extra_fields)
+    def create_user(self, username, email, password, **extra_fields):
+        return self._create_user(username, email, password, False, False, **extra_fields)
     
-    def create_staff(self, nickname, email, password, **extra_fields):
-        return self._create_user(nickname, email, password, True, False, **extra_fields)
+    def create_staff(self, username, email, password, **extra_fields):
+        return self._create_user(username, email, password, True, False, **extra_fields)
     
-    def create_superuser(self, nickname, email, password, **extra_fields):
-        return self._create_user(nickname, email, password, True, True, **extra_fields)
+    def create_superuser(self, username, email, password, **extra_fields):
+        return self._create_user(username, email, password, True, True, **extra_fields)
     
 class User(AbstractBaseUser):
-    nickname = models.CharField(unique=True, max_length=64)
+    username = models.CharField(unique=True, max_length=50)
     email = models.CharField(max_length=150, default='test@example.com')
     is_staff = models.BooleanField(default=False)
     is_superuser = models.BooleanField(default=False)
@@ -44,17 +44,16 @@ class User(AbstractBaseUser):
     last_login = models.DateTimeField(null=True, blank=True)
     date_joined = models.DateTimeField(auto_now_add=True)
 
-    USERNAME_FIELD = 'nickname'
+    USERNAME_FIELD = 'username'
     EMAIL_FIELD = 'email'
     REQUIRED_FIELDS = []
 
     objects = UserManager()
 
     def __str__(self):
-        return self.nickname
+        return self.username
     
 
 class Profile(models.Model):
     user = models.OneToOneField('User', on_delete=models.CASCADE)
     create_at = models.DateTimeField(auto_now_add=True)
-    # profileimg = models.ImageField()
