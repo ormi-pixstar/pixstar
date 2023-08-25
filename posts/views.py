7f7ed9e0aa8bfe90186999507b0850ce415ffaba
LJS0000 <jisu2281l@naver.com>
2023-08-25 14:58:00 +0900

    ✨ FEATURE: MVP 기능 1차 구현 완료
    버그, 예외처리, 비즈니스로직 관련 토의 후 수정 작업 진행

---
+++ posts/views.py
@@ -0,0 +1,186 @@
+from rest_framework.parsers import MultiPartParser
+from rest_framework.views import APIView
+from rest_framework.pagination import PageNumberPagination
+from rest_framework import status
+from rest_framework.response import Response
+from django.shortcuts import get_object_or_404
+import boto3
+import os
+from drf_spectacular.utils import extend_schema
+from .models import Post, Image, Comment
+from .serializers import (
+    PostSerializer,
+    ImageSerializer,
+    PostLikeSerializer,
+    CommentSerializer,
+    CommentCreateSerializer,
+)
+from django.db.models import Count
+from dotenv import load_dotenv
+
+# 환경변수 로드
+load_dotenv()
+
+
+class Pagination(PageNumberPagination):
+    page_size = 5  # 페이지당 보여줄 포스트 수
+    page_size_query_param = 'page_size'  # 페이지 크기를 지정하는 쿼리 파라미터
+    max_page_size = 100  # 최대 페이지 크기
+
+
+# 포스트 조회 및 검색
+class PostList(APIView):
+    pagination_class = Pagination
+
+    # 검색 쿼리 처리
+    def search_posts(self, search_query):
+        if search_query:
+            if search_query[0] == '@':  # user 검색
+                username = search_query[1:]
+                return Post.objects.filter(writer__username__icontains=username)
+            else:  # 포스트 검색
+                return Post.objects.filter(content__icontains=search_query)
+        return Post.objects.all()  # 전체 조회
+
+    # 정렬 쿼리 처리
+    def order_posts(self, posts, sort_order):
+        if sort_order == 'asc':
+            return posts.order_by('created_at')
+        elif sort_order == 'likes':
+            return posts.annotate(like_count=Count('like')).order_by('-like_count')
+        return posts.order_by('-created_at')
+
+    @extend_schema(responses=PostSerializer)
+    def get(self, request):
+        # 쿼리 파라미터 가져오기
+        search_query = request.query_params.get('search', None)
+        sort_order = request.query_params.get('sort', 'desc')
+
+        # 검색어에 맞는 포스트 가져오기
+        posts = self.search_posts(search_query)
+
+        # 조건에 맞춰서 정렬
+        posts = self.order_posts(posts, sort_order)
+
+        # 페이지 객체 생성 및 데이터 시리얼라이징
+        paginator = self.pagination_class()
+        page = paginator.paginate_queryset(posts, request)
+
+        serializer = PostSerializer(page, many=True)
+
+        response_data = {
+            'results': serializer.data,
+            'count': paginator.page.paginator.count,
+            'next': paginator.get_next_link(),
+            'previous': paginator.get_previous_link(),
+        }
+
+        return Response(response_data, status=status.HTTP_200_OK)
+
+
+class PostWrite(APIView):
+    def post(self, request):
+        serializer = PostSerializer(context={"request": request}, data=request.data)
+        if serializer.is_valid():
+            post = serializer.save(writer=request.user)
+            post.save()
+            return Response(status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class PostDetail(APIView):
+    def get(self, request, pk):
+        post = get_object_or_404(Post, pk=pk)
+        serializer = PostSerializer(post)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+class PostEdit(APIView):
+    def put(self, request, pk):
+        post = get_object_or_404(Post, pk=pk)
+        serializer = PostSerializer(
+            post, context={"request": request}, data=request.data
+        )
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class PostDelete(APIView):
+    def delete(self, request, pk):
+        post = get_object_or_404(Post, pk=pk)
+        post.delete()
+        return Response(status.HTTP_204_NO_CONTENT)
+
+
+class PostLike(APIView):
+    def get(self, request, pk):
+        post = get_object_or_404(Post, pk=pk)
+        serializer = PostLikeSerializer(post)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def put(self, request, pk):
+        post = get_object_or_404(Post, pk=pk)
+        if request.user in post.like.all():
+            post.like.remove(request.user)
+            return Response("unlike", status=status.HTTP_200_OK)
+        post.like.add(request.user)
+        return Response("like", status=status.HTTP_200_OK)
+
+
+# 이미지 업로드 TEST용, 배포 시 삭제
+class ImageUploadTest(APIView):
+    parser_classes = (MultiPartParser,)
+
+    @extend_schema(request={"file": {"type": "file"}}, responses={201: None})
+    def post(self, request):
+        file_obj = request.FILES.get('file')
+        # print(request.FILES)
+        if not file_obj:
+            return Response({"error": "파일이 없습니다."}, status=400)
+
+        bucket_name = os.getenv('AWS_STORAGE_BUCKET_NAME')
+        s3 = boto3.client('s3')
+        s3.upload_fileobj(file_obj, bucket_name, file_obj.name)
+
+        return Response(status=201)
+
+
+## 댓글 작성, 조회
+class CommentView(APIView):
+    def get(self, request, post_id):
+        post = Post.objects.get(id=post_id)
+        comments = post.comment_set.all()
+        serializer = CommentSerializer(comments, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def post(self, request, post_id):
+        serializer = CommentCreateSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save(writer=request.writer, post_id=post_id)
+            return Response(serializer.data, status.HTTP_201_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+## 댓글 수정, 삭제
+class CommentDetailView(APIView):
+    def put(self, request, post_id, comment_id):
+        comment = get_object_or_404(Comment, id=comment_id)
+        if request.user == comment.user:
+            serializer = CommentCreateSerializer(comment, data=request.data)
+            if serializer.is_valid():
+                serializer.save()  # post에 user 정보 있기 때문에 (user=request.user) 생략
+                return Response(serializer.data, status=status.HTTP_200_OK)
+            else:
+                return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        else:
+            return Response("권한이 없습니다.", status=status.HTTP_403_FORBIDDEN)
+
+    def delete(self, request, post_id, comment_id):
+        comment = get_object_or_404(Comment, id=comment_id)
+        if request.user == comment.user:
+            comment.delete()
+            return Response("삭제되었습니다.", status=status.HTTP_204_NO_CONTENT)
+        else:
+            return Response("권한이 없습니다.", status=status.HTTP_403_FORBIDDEN)
---
+++ users/views.py
@@ -0,0 +1,118 @@
+# Django
+from django.shortcuts import render, redirect
+from django.urls import reverse
+from django.views import View
+from django.contrib.auth import authenticate, login, logout
+
+# DjangoRestFramework
+from rest_framework import status, exceptions
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+# DjangroRestFramework-simpleJWT
+from rest_framework_simplejwt.serializers import (
+    TokenObtainPairSerializer,
+    TokenRefreshSerializer,
+)
+from rest_framework_simplejwt.tokens import RefreshToken
+
+# Custom
+from .serializers import (
+    SignupSerializer,
+    LoginSerializer,
+    SignoutSerializer,
+    ProfileSerializer,
+)
+
+
+### 회원가입
+class Signup(APIView):
+    def post(self, request):
+        serializer = SignupSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+            return Response("회원가입에 성공했습니다.", status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+### 회원탈퇴
+class Signout(APIView):
+    def post(self, request):
+        user = request.user
+
+        serializer = SignoutSerializer(data=request.data, context={"user": user})
+        if serializer.is_valid():
+            user.is_active = False
+            user.save()
+            return Response("회원탈퇴되었습니다.")
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+### 로그인
+class Login(APIView):
+    def post(self, request):
+        serializer = LoginSerializer(data=request.data)
+
+        if serializer.is_valid():
+            user = serializer.validated_data.get('user')
+            token = TokenObtainPairSerializer.get_token(user)
+            refresh_token = str(token)
+            access_token = str(token.access_token)
+
+            user = serializer.validated_data.get('user')
+            response = Response(
+                {
+                    "user": {"id": user.pk, "name": user.name},
+                    "message": "로그인 완료",
+                    "access_token": access_token,
+                },
+                status=status.HTTP_200_OK,
+            )
+
+            response.set_cookie(
+                "refresh", refresh_token, httponly=True, samesite='None', secure=True
+            )
+            return response
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+### 로그아웃
+class Logout(APIView):
+    def post(self, request):
+        refresh_token = request.COOKIES.get('refresh')
+        if refresh_token:
+            token = RefreshToken(refresh_token)
+            token.blacklist()
+        else:
+            return Response('비정상적인 토큰입니다.', status=status.HTTP_400_BAD_REQUEST)
+        response = Response('로그아웃 완료')
+        response.set_cookie('refresh', httponly=True, samesite='None', secure=True)
+        return response
+
+
+### 회원조회
+class UserDetail(APIView):
+    def get(self, request):
+        user = request.user
+        serializer = ProfileSerializer(user)
+        return Response(serializer.data)
+
+    def post(self, request):
+        user = request.user
+        serializer = ProfileSerializer(user, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+### 회원수정
+class UserUpdate(APIView):
+    def get(self, request):
+        pass
+
+    def post(self, request):
+        pass
