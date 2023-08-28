72497126641e4e388497700cc4224b6683778a2d
LJS0000 <jisu2281l@naver.com>
2023-08-28 14:45:53 +0900

    ✏️ FIX: 유저 모델 수정

--- users/models.py
+++ users/models.py
@@ -1,51 +1,50 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    BaseUserManager,
+    PermissionsMixin,
+)
 from django.utils import timezone
 
 
 class UserManager(BaseUserManager):
-    def _create_user(
-        self, name, email, password, is_staff, is_superuser, **extra_fields
-    ):
+    def _create_user(self, email, password, is_staff, is_superuser, **extra_fields):
         if not email:
-            raise ValueError('User must have an email')
+            raise ValueError('이메일은 필수입니다.')
         now = timezone.localtime()
         email = self.normalize_email(email)
         user = self.model(
-            name=name,
             email=email,
             is_staff=is_staff,
             is_superuser=is_superuser,
             date_joined=now,
             **extra_fields
         )
-
         user.set_password(password)
         user.save(using=self.db)
         return user
 
-    def create_user(self, name, email, password, **extra_fields):
-        return self._create_user(name, email, password, False, False, **extra_fields)
+    def create_user(self, email, password, **extra_fields):
+        return self._create_user(email, password, False, False, **extra_fields)
 
-    def create_superuser(self, name, email, password, **extra_fields):
-        return self._create_user(name, email, password, True, True, **extra_fields)
+    def create_superuser(self, email, password, **extra_fields):
+        return self._create_user(email, password, True, True, **extra_fields)
 
 
-class User(AbstractBaseUser):
-    name = models.CharField(unique=True, max_length=64)
-    email = models.EmailField(max_length=155)
-    profileimg = models.ImageField(null=True, blank=True)
+class User(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(unique=True, max_length=155)
+    profile_img = models.ImageField(null=True, blank=True)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
     is_superuser = models.BooleanField(default=False)
     date_joined = models.DateTimeField(auto_now_add=True)
-    last_login = models.DateTimeField(null=True, blank=True)
-
-    USERNAME_FIELD = 'name'
-    EMAIL_FIELD = 'email'
-    REQUIRED_FIELDS = ['email']
+    last_login = models.DateTimeField(auto_now=True, null=True, blank=True)
 
     objects = UserManager()
 
+    USERNAME_FIELD = 'email'
+    EMAIL_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
     def __str__(self):
-        return self.name
+        return self.email
