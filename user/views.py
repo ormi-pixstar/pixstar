843150229acee3d4ae4a06950f5c429770bade76
LJS0000 <jisu2281l@naver.com>
2023-08-29 11:05:56 +0900

    ✏️ FIX: 사용 안 하는 모듈 삭제

--- user/views.py
+++ user/views.py
@@ -2,7 +2,6 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
-from rest_framework.exceptions import AuthenticationFailed
 from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework_simplejwt.exceptions import TokenError
 from rest_framework_simplejwt.authentication import JWTAuthentication
