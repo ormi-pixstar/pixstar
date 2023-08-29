927d3aafee9997f0896fe4cc922033c1b39bb707
LJS0000 <jisu2281l@naver.com>
2023-08-29 15:12:08 +0900

    ⚡️ IMPRV: utility function 파일 분리

--- post/serializers.py
+++ post/serializers.py
@@ -3,18 +3,11 @@ from rest_framework.response import Response
 from rest_framework import status
 from .models import Post, Image, Like, Comment
 from django.contrib.auth import get_user_model
+from .utils import unique_filename
 import os
 import boto3
-import uuid
 
 User = get_user_model()
-
-
-# 파일명이 중복되는 경우를 방지
-def unique_filename(instance, filename):
-    ext = filename.split('.')[-1]
-    new_filename = f"{uuid.uuid4()}.{ext}"
-    return f'images/{new_filename}'
 
 
 class ImageSerializer(serializers.ModelSerializer):
@@ -88,16 +81,50 @@ class PostSerializer(serializers.ModelSerializer):
             'likes',
         ]
 
+    # 포스트 조회 시에만 writer 항목을 추가
     def to_representation(self, instance):
         representation = super(PostSerializer, self).to_representation(instance)
         representation['writer'] = instance.writer.email
         return representation
 
     def create(self, validated_data):
-        writer = self.context.get('writer', None)
+        post_instance = self._create_post(validated_data)
+        self._validate_and_save_images(post_instance)
+        return post_instance
+
+    # 포스트 생성
+    def _create_post(self, validated_data):
+        writer = self.context.get('writer')
         if writer:
-            self.validated_data['writer'] = writer
-        return super(PostSerializer, self).create(validated_data)
+            validated_data['writer'] = writer
+        return super().create(validated_data)
+
+    # 이미지 저장
+    def _validate_and_save_images(self, post_instance):
+        images_data = self.context['request'].FILES.getlist('images')
+
+        self._check_images_count(images_data)
+
+        image_data_list = [
+            {'post': post_instance.id, 'image': image} for image in images_data
+        ]
+        image_serializer = ImageSerializer(
+            data=image_data_list, many=True, context=self.context
+        )
+
+        if image_serializer.is_valid():
+            image_serializer.save(post=post_instance)
+        else:
+            raise serializers.ValidationError(image_serializer.errors)
+
+    # 이미지 개수 검증
+    def _check_images_count(self, images_data):
+        if len(images_data) == 0:
+            raise serializers.ValidationError(
+                {'detail': 'At least 1 image should be uploaded.'}
+            )
+        elif len(images_data) > 10:
+            raise serializers.ValidationError({'detail': 'Maximum 10 files allowed.'})
 
 
 class UserPostSerializer(serializers.ModelSerializer):
