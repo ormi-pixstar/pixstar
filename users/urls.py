0bde2fb49af0eec992b36bb5bb188bb6b7317c0e
LJS0000 <jisu2281l@naver.com>
2023-08-31 23:02:48 +0900

    ✏️ FIX: 뷰 클래스명 수정

--- users/urls.py
+++ users/urls.py
@@ -5,15 +5,15 @@ app_name = 'users'
 
 urlpatterns = [
     # 회원가입
-    path('signup/', views.Signup.as_view(), name='Signup'),
-    # 회원탈퇴
-    path('signout/', views.Signout.as_view(), name='signout'),
+    path('signup/', views.SignupView.as_view(), name='Signup'),
     # 로그인
-    path('login/', views.Login.as_view(), name='Login'),
+    path('login/', views.LoginView.as_view(), name='Login'),
     # 로그아웃
-    path('logout/', views.Logout.as_view(), name='Logout'),
+    path('logout/', views.LogoutView.as_view(), name='Logout'),
+    # 회원탈퇴
+    path('signout/', views.SignoutView.as_view(), name='signout'),
     # 회원정보 조회
-    path('profile/', views.UserDetail.as_view(), name='UserDetail'),
+    path('profile/', views.UserDetailView.as_view(), name='UserDetail'),
     # 회원정보 수정
-    path('update/', views.UserUpdate.as_view(), name='update'),
+    path('update/', views.UserUpdateView.as_view(), name='update'),
 ]
