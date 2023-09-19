25c4a6973dccc5fd50c035d907cfc4cf694ac1c7
LJS0000 <jisu2281l@naver.com>
2023-09-19 13:31:03 +0900

    🚧 WIP: routing, consumers 파일 생성

---
+++ chat/routing.py
@@ -0,0 +1,9 @@
+from channels.routing import ProtocolTypeRouter, URLRouter
+from django.urls import path
+from chat import consumers
+
+websocket_urlpatterns = [
+    path('ws/chat/<room_name>/', consumers.ChatConsumer.as_asgi()),
+]
+
+application = ProtocolTypeRouter({'websocket': URLRouter(websocket_urlpatterns)})
